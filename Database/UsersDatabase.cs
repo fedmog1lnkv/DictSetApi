a39c0420dde4dca9aec33e63375da158632a3953
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-03-29 20:49:28 +0800

    User database class
    The User class for working with the Users table.
    UnitTests are included.

---
+++ Database/UsersDatabase.cs
@@ -0,0 +1,77 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Database;
+
+public class UsersDatabase : DBClass
+{
+    
+    public bool Register(string username, string email, string password)
+    {
+        if (DBUtils.CheckUserEmailExists(email))
+        {
+            // A user with this name exists
+            return false;
+        }
+
+        string sql = $"INSERT INTO users (username, email, password) values ('{username}', '{email}', '{password}');";
+
+        MySqlConnection connection = GetDBConnection();
+        connection.Open();
+        MySqlCommand cmd = connection.CreateCommand();
+        cmd.CommandText = sql;
+        cmd.ExecuteNonQuery();
+        connection.Close();
+        
+        // User created
+        return true;
+    }
+
+    public bool DeleteUser(string email)
+    {
+        string sql = $"DELETE FROM users WHERE email = '{email}' LIMIT 1;";
+
+        if (!DBUtils.CheckUserEmailExists(email))
+        {
+            // A user with this name not exists
+            return false;
+        }
+
+        MySqlConnection connection = GetDBConnection();
+        connection.Open();
+        MySqlCommand cmd = connection.CreateCommand();
+        cmd.CommandText = sql;
+        cmd.ExecuteNonQuery();
+        connection.Close();
+
+        // User deleted
+        return true;
+    }
+
+    public bool Authorization(string email, string password)
+    {
+        string sql = $"SELECT COUNT(*) FROM users WHERE email = '{email}' AND password = '{password}'";
+        
+        MySqlConnection connection = DBClass.GetDBConnection();
+        connection.Open();
+        MySqlCommand cmd = connection.CreateCommand();
+        cmd.CommandText = sql;
+        long count = (long)cmd.ExecuteScalar();
+        Console.WriteLine(count);
+        
+        return count == 1;
+    }
+
+    public int GetUserId(string email)
+    {
+        string sql = $"SELECT user_id FROM users WHERE email = '{email}'";
+
+        MySqlConnection connection = GetDBConnection();
+        connection.Open();
+        MySqlCommand cmd = connection.CreateCommand();
+        cmd.CommandText = sql;
+        int user_id = Int32.Parse(cmd.ExecuteScalar().ToString());
+        connection.Close();
+
+        return user_id;
+    }
+}
---
+++ TestsDBClasses/UnitTestForUserDatabase.cs
@@ -0,0 +1,77 @@
+namespace Database;
+
+[TestClass]
+public class UnitTestRegister
+{
+    UsersDatabase db = new UsersDatabase();
+    string username = "fedm";
+    string email = "fedm@yandex.ru";
+    string password = "1234";
+
+    [TestMethod]
+    public void TestRegister()
+    {
+        db.DeleteUser(email);
+        bool actual = db.Register(username, email, password);
+        Assert.AreEqual(true, actual);
+        db.DeleteUser(email);
+    }
+
+    [TestMethod]
+    public void TestRegisterWithExistUser()
+    {
+        bool actual = db.Register(username, email, password);
+        actual = db.Register(username, email, password);
+        Assert.AreEqual(false, actual);
+    }
+}
+
+[TestClass]
+public class UnitTestDelete
+{
+    UsersDatabase db = new UsersDatabase();
+    string username = "fedm";
+    string email = "fedm@yandex.ru";
+    string password = "1234";
+
+    [TestMethod]
+    public void TestDelete()
+    {
+        db.Register(username, email, password);
+        bool actual = db.DeleteUser(email);
+        Assert.AreEqual(true, actual);
+    }
+
+    [TestMethod]
+    public void TestDeleteWithNotExistUser()
+    {
+        bool actual = db.DeleteUser(email);
+        Assert.AreEqual(false, actual);
+    }
+}
+
+
+[TestClass]
+public class UnitTestAuthorization
+{
+    UsersDatabase db = new UsersDatabase();
+    string username = "fedm";
+    string email = "fedm@yandex.ru";
+    string password = "1234";
+    
+    [TestMethod]
+    public void TestAuthorization()
+    {
+        db.Register(username, email, password);
+        bool actual = db.Authorization(email, password);
+        Assert.AreEqual(true, actual);
+        db.DeleteUser(email);
+    }
+
+    [TestMethod]
+    public void TestAuthorizationWithNotExistUser()
+    {
+        bool actual = db.Authorization(email, password);
+        Assert.AreEqual(false, actual);
+    }
+}
